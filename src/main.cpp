25f63e205239161fedade09d9bf9ec76bb3ee7d6
Borys Cywinski <cyviloo@gmail.com>
2017-10-10 11:43:19 +0200

    Adopt and run first .a library

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include "geometry.h"
 #include "math.h"
+
+/* Dodajemy naglowek biblioteki factorial.a ! */
+#include "factorial.h"
+
 using namespace std;
 
 int main(int argc, char **argv) {
 	cout << "Ciezka matematyka...\n5 + 50 = " 
 	<< add(5, 50) << "\nPole kola o promieniu 10 = "
-	<< circleArea(10) << endl;
+	<< circleArea(10) << endl << endl;
+
+	cout << "Uwaga, a oto wykonanie funkcji z biblioteki"
+	" factorial.a:\nsilnia z 10 = " << factorial(10) << endl;
 	return 0;
 }
 
